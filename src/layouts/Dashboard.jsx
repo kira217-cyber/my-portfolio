f7a651fd28716411c686ff55dadfb000ba4d6ef3
kira217-cyber <gamingkira217@gmail.com>
2025-08-11 01:48:04 +0600

    one last problem solve it

--- src/layouts/Dashboard.jsx
+++ src/layouts/Dashboard.jsx
@@ -4,6 +4,7 @@ import Navbar from '../pages/Shared/Navbar/Navbar';
 import Footer from '../pages/Shared/Footer/Footer';
 import ScrollToTopButton from '../components/ScrollToTopButton/ScrollToTopButton';
 import DashBoardNav from '../components/DashBoardNav/DashBoardNav';
+import ScrollToTop from '../components/ScrollToTop/ScrollToTop';
 
 const Dashboard = () => {
     return (
@@ -12,6 +13,7 @@ const Dashboard = () => {
             <Outlet></Outlet>
             <Footer></Footer>
             <ScrollToTopButton></ScrollToTopButton>
+            <ScrollToTop />
         </div>
     );
 };
--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -3,6 +3,7 @@ import { Outlet } from 'react-router';
 import Navbar from '../pages/Shared/Navbar/Navbar';
 import Footer from '../pages/Shared/Footer/Footer';
 import ScrollToTopButton from '../components/ScrollToTopButton/ScrollToTopButton';
+import ScrollToTop from '../components/ScrollToTop/ScrollToTop';
 
 const RootLayout = () => {
     return (
@@ -11,6 +12,7 @@ const RootLayout = () => {
             <Outlet></Outlet>
             <Footer></Footer>
             <ScrollToTopButton></ScrollToTopButton>
+            <ScrollToTop />
         </div>
     );
 };
